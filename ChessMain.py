a667d31b6e99e70232388907a4ee4bd787e1bd46
PesAdam <pesadam15@gmail.com>
2022-04-04 00:04:46 +0200

    no stava sa  nam z toho guca sraciek

--- ChessMain.py
+++ ChessMain.py
@@ -17,7 +17,7 @@ IMAGES = {}                     # pre ukladanie obrazkov
 def load_images():
     pieces = ['wp, wN, wB, wR, wQ, wK, bp, bN, bB, bR, bQ, bK']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load(f'img/${piece}.png'), (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load('./img/'+ piece +'.png'), (SQ_SIZE, SQ_SIZE))
     #transform sluzi nato aby obrazok vzdy vyzeral dobre
     # mame pristup k obrazkom, vieme sa k nim dostat cez 'IMAGES['wp']'
 
@@ -29,3 +29,37 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState() #vytvorime instanciu hry, gs = game state
+    load_images()               #nacitame obrzky, robime to iba raz!!!
+    running = True              #bezime? bezime!
+    while running:                  #ak bezi tak sa pozre ci nahodou neni niekde beziaci pygame event
+        for e in p.event.get():     #ak je niekde event, tak ho vypne
+            if e.type == p.QUIT:    #ak je to quit event, tak sa ukonci
+                running = False     
+        draw_game_state(screen, gs) #vykreslime stav hry
+        clock.tick(MAX_FPS)         #nastavime fps
+        p.display.flip()            #vykresli hru 
+
+#Zodpovedna za vsetky kreslenia
+
+def draw_game_state(screen, gs):
+    drawBoard(screen)               #nakreslime hraciu plochu
+    
+    drawPieces(screen, gs.board)    #nakreslime figurky
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]    #dame sivu aby sme tie cierne figurky potom videli
+    for i in range(DIMENSION):                      #pre kazdy riadok      
+        for j in range(DIMENSION):                  #pre kazdy stlpec
+            color = colors[(i + j) % 2]             #dame farbu podla pocitadla
+            p.draw.rect(screen, color, p.Rect(i * SQ_SIZE, j * SQ_SIZE, SQ_SIZE, SQ_SIZE)) #vykreslime poziciu
+
+
+# nakreslime figurky pomocou aktualne gamestate.board
+def drawPieces(screen, board):
+    pass
+
+
+    
+if __name__ == "__main__":
+    main()
